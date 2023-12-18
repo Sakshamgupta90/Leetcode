880c3f96921759880e77e8916425e0b96bd50d95
Saksham Gupta <84325475+Sakshamgupta90@users.noreply.github.com>
2023-12-19 00:10:11 +0530

    Time: 0 ms (100%), Space: 38.8 MB (93.83%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -6,10 +6,18 @@ class Solution {
 
         return dp[n] = stairs_memo(n-1, dp) + stairs_memo(n-2, dp);
     }
+    private int stairs_tabu(int n, int[] dp){
+        dp[0] = dp[1] = 1;
+        for(int i =2; i <= n; i++){
+            dp[i] = dp[i-1]  + dp[i-2];
+        }
+        return dp[n];
+    }
     public int climbStairs(int n) {
         int[] dp = new int[n+1];
         Arrays.fill(dp, -1);
 
-        return stairs_memo(n, dp);
+        // return stairs_memo(n, dp);
+        return stairs_tabu(n, dp);
     }
 }
