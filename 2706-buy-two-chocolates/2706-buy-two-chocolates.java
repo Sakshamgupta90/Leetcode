d4abca487455d283224094958bdf479ca5083653
Saksham Gupta <84325475+Sakshamgupta90@users.noreply.github.com>
2023-12-21 01:22:06 +0530

    Time: 1 ms (100%), Space: 44.5 MB (5.58%) - LeetHub

--- 2706-buy-two-chocolates/2706-buy-two-chocolates.java
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.java
@@ -1,4 +1,13 @@
 class Solution {
+    // **************Approach1 - Brute force -> TC: O(NlogN)*******************
+    // public int buyChoco(int[] prices, int money) {
+    //     Arrays.sort(prices);
+    //     int totalPrice = prices[0] + prices[1];
+
+    //     return totalPrice > money ? money : money - totalPrice;
+
+    // }
+    // *******************Approach2 - optimise -> TC : O(N)*********************
     public int buyChoco(int[] prices, int money) {
       int first_min = Integer.MAX_VALUE, second_min = Integer.MAX_VALUE;
 
