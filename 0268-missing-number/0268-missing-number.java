86266662ab525995b6e733802ad23a02784ddb58
Saksham Gupta <84325475+Sakshamgupta90@users.noreply.github.com>
2023-12-31 00:22:17 +0530

    Time: 8 ms (6.26%), Space: 45.8 MB (5.03%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,10 +1,20 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        int sum = 0;
-       for(int i = 0; i < nums.length; i++){
-           sum += i;
-           sum -= nums[i];
-       }
-        return sum + nums.length;
+        /** Approach1: Using HashSet: We add all the elements of the array into
+        the hashset. Then we iterate through our array and see if that ith
+        element is present or not in out hashset. If its not then
+        will return that num. */
+
+        HashSet<Integer> set = new HashSet<>();
+        for(int ele: nums)
+            set.add(ele);
+        
+        for(int i = 0; i <= nums.length; i++){
+            if(!set.contains(i))
+                return i;
+
+        }
+
+        return -1;
     }
 }
