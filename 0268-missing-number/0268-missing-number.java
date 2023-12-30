fefb92f56e846ac159fec8c8b63dc88743a60dde
Saksham Gupta <84325475+Sakshamgupta90@users.noreply.github.com>
2023-12-31 00:53:05 +0530

    Time: 1 ms (41.06%), Space: 45.3 MB (6.86%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,20 +1,37 @@
 class Solution {
-    public int missingNumber(int[] nums) {
-        /** Approach1: Using HashSet: We add all the elements of the array into
-        the hashset. Then we iterate through our array and see if that ith
-        element is present or not in out hashset. If its not then
-        will return that num. */
-
-        HashSet<Integer> set = new HashSet<>();
-        for(int ele: nums)
-            set.add(ele);
-        
-        for(int i = 0; i <= nums.length; i++){
-            if(!set.contains(i))
-                return i;
-
+    public int missingNumber(int[]nums){
+        /** Optimal Approach: Summation
+        eg: given arr: [3,0,1] and the range is [0,1,2,3]
+        so if we add each number of arr i.e 3+0+1 = 4 and add each number 
+        of the range by using the formula(sum of 1st n terms) n*(n+1)/2 
+        And subtract them 6-4 = 2 we get our missing number */
+        int rangeSum = 0, count = 0;
+        int n = nums.length;
+        rangeSum = (n*(n+1))/2;
+        for(int i = 0; i < nums.length; i++){
+            count += nums[i];
+            
         }
 
-        return -1;
+        return rangeSum - count;
     }
+    // public int missingNumber(int[] nums) {
+    //     /** Approach1: Using HashSet: We add all the elements of the array   
+    //     in the hashset. Then we iterate through our array and see if that ith
+    //     element is present or not in out hashset. If its not then
+    //     will return that num. */
+
+    //     HashSet<Integer> set = new HashSet<>();
+    //     for(int ele: nums)
+    //         set.add(ele);
+        
+    //     for(int i = 0; i <= nums.length; i++){
+    //         if(!set.contains(i))
+    //             return i;
+
+    //     }
+
+    //     return -1;
+    //      TC: O(N) + O(N) ~ O(N)  SC: O(N)
+    // }
 }
