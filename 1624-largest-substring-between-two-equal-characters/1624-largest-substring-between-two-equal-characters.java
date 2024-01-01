50d3c321aced50e5fde4262ed01b8e28525a703f
Saksham Gupta <84325475+Sakshamgupta90@users.noreply.github.com>
2024-01-01 15:15:08 +0530

    Time: 2 ms (56.36%), Space: 41.7 MB (7.48%) - LeetHub

--- 1624-largest-substring-between-two-equal-characters/1624-largest-substring-between-two-equal-characters.java
+++ 1624-largest-substring-between-two-equal-characters/1624-largest-substring-between-two-equal-characters.java
@@ -1,23 +1,42 @@
 class Solution {
-    public int maxLengthBetweenEqualCharacters(String s) {
-
+    public int maxLengthBetweenEqualCharacters(String s){
         if(s.length() == 1)
             return -1;
-
-        int len = Integer.MIN_VALUE;
+        
+        HashMap<Character, Integer> map = new HashMap<>();
+        int len = -1;
         for(int i = 0; i < s.length(); i++){
             char ch = s.charAt(i);
-            for(int j = i+1; j < s.length(); j++){
-                char ch2 = s.charAt(j);
-                if(ch == ch2){
-                    len = Math.max(len, j-i-1);
-                }
+            if(map.containsKey(ch)){
+                int j = map.get(ch);
+                len = Math.max(len, i-j-1);
+
+            }else{
+                map.put(ch, i);
             }
         }
 
-        if(len == Integer.MIN_VALUE)
-            return -1;
-
         return len;
     }
+    // public int maxLengthBetweenEqualCharacters(String s) {
+
+    //     if(s.lengh() == 1)
+    //         return -1;
+
+    //     int len = Integer.MIN_VALUE;
+    //     for(int i = 0; i < s.length(); i++){
+    //         char ch = s.charAt(i);
+    //         for(int j = i+1; j < s.length(); j++){
+    //             char ch2 = s.charAt(j);
+    //             if(ch == ch2){
+    //                 len = Math.max(len, j-i-1);
+    //             }
+    //         }
+    //     }
+
+    //     if(len == Integer.MIN_VALUE)
+    //         return -1;
+
+    //     return len;
+    // }
 }
