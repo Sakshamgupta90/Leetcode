2e357e69be1e0fa1faa75b22acb17156de568596
Saksham Gupta <84325475+Sakshamgupta90@users.noreply.github.com>
2024-01-06 00:48:40 +0530

    Time: 7 ms (79.34%), Space: 44.6 MB (15.94%) - LeetHub

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int lengthOfLIS(int[] arr) {
+        List<Integer> temp = new ArrayList<>();
+        temp.add(arr[0]);
+        int n = arr.length;
+        int len = 1;
+
+        for (int i = 1; i < n; i++) {
+            if (arr[i] > temp.get(temp.size() - 1)) {
+                // arr[i] > the last element of temp array
+
+                temp.add(arr[i]);
+                len++;
+            } else {
+                // Replacement step
+                int ind = Collections.binarySearch(temp, arr[i]);
+
+                if (ind < 0) {
+                    ind = -ind - 1;
+                }
+
+                temp.set(ind, arr[i]);
+            }
+        }
+
+        return len;
+    }
+}
