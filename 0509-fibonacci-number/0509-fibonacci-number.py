96e01f2d2031fc75e1aa200ebb628b64f9add03a
Saksham Gupta <84325475+Sakshamgupta90@users.noreply.github.com>
2024-05-15 23:09:36 +0530

    Time: 327 ms (28.53%), Space: 16.6 MB (26.72%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,13 +1,11 @@
 class Solution:
     def fib(self, n: int) -> int:
-        if n == 0 or n == 1:
+        if n <= 1:
             return n
-        a,b = 0,1
-        for i in range(1,n):
-            sum = a+b
-            a = b
-            b = sum
-        return b
+        
+        return self.fib(n-1) + self.fib(n-2)
+            
+            
         
 
 
